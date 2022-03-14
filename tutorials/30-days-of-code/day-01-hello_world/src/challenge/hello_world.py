57a974cba3f5043795cd0169d9e7a67765629f82
Victor Payno <vpayno@users.noreply.github.com>
2022-03-14 13:37:02 -0700

    30-days-of-code/day-01: 1st iteration

--- tutorials/30-days-of-code/day-01-hello_world/src/challenge/hello_world.py
+++ tutorials/30-days-of-code/day-01-hello_world/src/challenge/hello_world.py
@@ -1,7 +1,65 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-input_string = input()
+"""
+HackerRank - 30 Days of Code - Day 01 - HelloWorld
 
-# Print a string literal saying "Hello, World." to stdout.
-print("Hello, World.")
+Author: Victor Payno (https://github.com/vpayno/hackerrank-workspace)
+"""
 
-# TODO: Write a line of code here that prints the contents of input_string to stdout.
+import sys
+
+
+class Challenge:
+    """
+    Day 01 - HelloWorld
+    """
+
+    def __init__(self, greeting="Hello, World."):
+        """
+        The things we do to make pylink happy.
+        """
+
+        self.greeting: str = greeting
+        self.input_string: str = None
+
+    def __str__(self):
+        """
+        String representation of the class.
+        """
+
+        output: str = None
+
+        if self.input_string:
+            output = self.greeting + " " + self.input_string
+        else:
+            output = self.greeting
+
+        return output
+
+    def hello_world(self, input_string=None):
+        """
+        Read a full line of input from stdin and save it to our dynamically
+        typed variable, input_string.
+
+        Print a string literal saying "Hello, World." to stdout.
+
+        Print the user supplied string.
+        """
+
+        if input_string:
+            self.input_string = input_string
+        else:
+            self.input_string = input()
+
+        print(self.greeting)
+
+        print(self.input_string)
+
+        return [self.greeting, self.input_string]
+
+
+if __name__ == "__main__":  # pragma: no cover
+
+    user_input: str = None if sys.argv[1] is None else sys.argv[1]
+
+    challenge = Challenge()
+
+    challenge.hello_world(input_string=user_input)
---
+++ tutorials/30-days-of-code/day-01-hello_world/src/test/test_hello_world.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""
+https://www.hackerrank.com/challenges/30-hello-world/problem
+
+Author: Victor Payno (https://github.com/vpayno)
+
+Challenge Tests
+"""
+
+import builtins
+import os.path
+import subprocess
+import sys
+from typing import List
+
+import mock
+import pytest
+
+# Our Project
+from challenge import hello_world
+
+unit_test_data = [
+    [
+        "Hello pytest!",
+        "Let's run some tests!",
+        ["Hello pytest!", "Let's run some tests!"],
+    ],
+]
+
+integration_test_data = [
+    [
+        "Let's run some tests!",
+        ["Hello, World.", "Let's run some tests!"],
+    ],
+]
+
+
+def test_class_init():
+    """the things we test for 100% coverage without using nocover"""
+    program: hello_world.Challenge = hello_world.Challenge(
+        greeting="greeting text")
+    expected_output: List[str] = ["greeting text", "user input"]
+    program_output: List[str] = program.hello_world(input_string="user input")
+    # assert len(expected_output) == len(program_output)
+    assert all(e == o for e, o in zip(expected_output, program_output))
+
+
+def test_class_str():
+    """test for the __str__ method that was added because pylint loves to complain"""
+    assert hello_world.Challenge().__str__() == "Hello, World."
+    assert hello_world.Challenge(
+        greeting="Hello pytest!").__str__() == "Hello pytest!"
+
+
+@pytest.mark.parametrize("greeting,input_string,expected", unit_test_data)
+def test_method_without_input(greeting: str, input_string: str,
+                              expected: List[str]):
+    """Runs the hello_world class method against all of our test data."""
+    code: hello_world.Challenge = hello_world.Challenge(greeting)
+    result: str = code.hello_world(input_string)
+    assert result == expected
+    assert code.__str__() == " ".join(expected)
+
+
+@pytest.mark.parametrize("greeting,input_string,expected", unit_test_data)
+def test_method_with_input(greeting: str, input_string: str,
+                           expected: List[str]):
+    """Runs the hello_world class method against all of our test data."""
+    code: hello_world.Challenge = hello_world.Challenge(greeting)
+    with mock.patch.object(builtins, "input", lambda: input_string):
+        result: str = code.hello_world()
+    assert result == expected
+    assert code.__str__() == " ".join(expected)
+
+
+@pytest.mark.parametrize("input_string,expected", integration_test_data)
+def test_script(input_string: str, expected: List[str]):
+    """Runs the hello_world script against all of our test data."""
+    process = subprocess.run(
+        [
+            sys.executable,
+            os.path.join(os.path.dirname("src/challenge/"), "hello_world.py"),
+            input_string,
+        ],
+        check=False,
+        stdout=subprocess.PIPE,
+    )
+    print(f"{process.stdout.decode('utf-8')} == {expected}")
+    program_output: List[str] = process.stdout.decode("utf-8")
+    assert expected[0] in program_output
+    assert expected[1] in program_output
+    assert all(e == o for e, o in zip(expected, program_output.split("\n")))
