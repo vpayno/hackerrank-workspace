415fd8b4c04c9919b3076448a57a3c7659a1bda2
Victor Payno <vpayno@users.noreply.github.com>
2022-03-14 23:58:55 -0700

    prep-kit-3-months/week01/day01-plus_minus: 1st iteration - 99% coverage

--- prep-kit-3-months/week01/day01-plus_minus/src/challenge/main.py
+++ prep-kit-3-months/week01/day01-plus_minus/src/challenge/main.py
@@ -1,23 +1,89 @@
 #!/bin/python3
+"""
+https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem
 
-import math
-import os
-import random
-import re
+Author: Victor Payno (https://github.com/vpayno/hackerrank-workspace)
+"""
+
+import collections
 import sys
+from typing import List, Optional, OrderedDict
 
-#
-# Complete the 'plusMinus' function below.
-#
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
 
-def plusMinus(arr):
-    # Write your code here
+class Challenge:
+    """
+    Week 1 - Plus Minus
+    """
 
-if __name__ == '__main__':
-    n = int(input().strip())
+    def __init__(self):
+        """
+        Set the precision to 6.
+        """
 
-    arr = list(map(int, input().rstrip().split()))
+    def __str__(self) -> str:
+        """
+        String representation of the class.
+        """
 
-    plusMinus(arr)
+        return self.__class__.__name__
+
+    def plusMinus(self, arr: Optional[List[int]]) -> List[float]:
+        """
+        Given an array of integers, calculate the ratios of its elements
+        that are positive, negative, and zero. Print the decimal value of
+        each fraction on a new line with 6 places after the decimal.
+        """
+        # pylint: disable=invalid-name,no-self-use
+
+        # added in-line if to fix:
+        # error: Argument 1 to "len" has incompatible type "Optional[List[int]]"; expected "Sized"
+        size: int = len(arr) if arr is not None else 0
+
+        counter: OrderedDict[str, int] = collections.OrderedDict({
+            "positive": 0,
+            "negative": 0,
+            "zero": 0
+        })
+
+        result: List[float] = []
+
+        number: float = 0.0
+
+        # added or [] to fix:
+        # error: Item "None" of "Optional[List[int]]" has no attribute "__iter__" (not iterable)
+        for number in arr or []:
+            if number > 0:
+                counter["positive"] += 1
+            elif number < 0:
+                counter["negative"] += 1
+            else:
+                counter["zero"] += 1
+
+        for _, value in counter.items():
+            result.append(value / size)
+
+        for number in result:
+            print(f"{number:.6f}")
+
+        return result
+
+
+if __name__ == "__main__":  # pragma: nocover
+    input_size: Optional[int] = None if sys.argv[1] is None else int(
+        sys.argv[1])
+    user_input: Optional[List[int]] = (None if sys.argv[2] is None else
+                                       [int(_) for _ in sys.argv[2].split(" ")])
+
+    length: Optional[int] = 0
+    array: Optional[List[int]] = []
+
+    if not input_size and not user_input:
+        length = int(input().strip())
+        array = list(map(int, input().rstrip().split()))
+    else:
+        length = input_size
+        array = user_input
+
+    program = Challenge()
+
+    output: List[float] = program.plusMinus(array)
---
+++ prep-kit-3-months/week01/day01-plus_minus/src/test/test_main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""
+Author: Victor Payno (https://github.com/vpayno)
+
+Challenge Tests
+"""
+
+import builtins
+import os.path
+import subprocess
+import sys
+from decimal import Decimal, getcontext
+from typing import List
+
+import mock
+import pytest
+
+# Our Project
+from challenge import main
+
+unit_test_data = [
+    [
+        5,
+        [-1, 1, 1, 0, -1],
+        [0.400000, 0.400000, 0.200000],
+    ],
+    [
+        6,
+        [-4, 3, -9, 0, 4, 1],
+        [0.500000, 0.333333, 0.166667],
+    ],
+]
+
+integration_test_data = [
+    [
+        "5",
+        "-1 1 1 0 -1",
+        [
+            "0.400000",
+            "0.400000",
+            "0.200000",
+        ],
+    ],
+]
+
+
+def test_class_str():
+    """test for the __str__ method that was added because pylint loves to complain"""
+    assert main.Challenge().__str__() == "Challenge"
+
+
+@pytest.mark.parametrize("size,array,expected", unit_test_data)
+def test_method_without_input(size: int, array: List[int],
+                              expected: List[float]):
+    """Runs the challenge class method against all of our test data."""
+    float1: float
+    float2: float
+
+    code: main.Challenge = main.Challenge()
+    result: List[float] = code.plusMinus(array)
+
+    assert size > 0
+    for float1, float2 in zip(result, expected):
+        print(f"{float1} == {float2}")
+        assert f"{float1:.6f}" == f"{float2:.6f}"
+
+
+@pytest.mark.parametrize("size,array,expected", unit_test_data)
+def test_method_with_input(size: str, array: str, expected: List[float]):
+    """Runs the main class method against all of our test data."""
+    getcontext().prec = 6
+
+    float1: float
+    float2: float
+    decimal1: Decimal
+    decimal2: Decimal
+
+    code: main.Challenge = main.Challenge()
+
+    with mock.patch.object(builtins, "input", lambda: size):
+        result: List[float] = code.plusMinus(array)
+
+    assert size > 0
+    for float1, float2 in zip(result, expected):
+        decimal1 = Decimal(float1) / Decimal(1)
+        decimal2 = Decimal(float2) / Decimal(1)
+        print(f"{decimal1} == {decimal2}")
+        assert decimal1 == decimal2
+
+
+@pytest.mark.parametrize("size,array,expected", integration_test_data)
+def test_script(size: int, array: str, expected: List[str]):
+    """Runs the main script against all of our test data."""
+    process = subprocess.run(
+        [
+            sys.executable,
+            os.path.join(os.path.dirname("src/challenge/"), "main.py"),
+            size,
+            array,
+        ],
+        check=False,
+        stdout=subprocess.PIPE,
+    )
+    print(f"{process.stdout.decode('utf-8')} == {expected}")
+    program_output: List[str] = process.stdout.decode("utf-8")
+    assert expected[0] in program_output
+    assert expected[1] in program_output
+    assert all(e == o for e, o in zip(expected, program_output.split("\n")))
