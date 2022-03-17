72a24a5419ef86660fb14798077ca2d9bb675a01
Victor Payno <vpayno@users.noreply.github.com>
2022-03-16 21:17:32 -0700

    30-days-of-code/day-03-operators: 1st iteration - submitted

--- tutorials/30-days-of-code/day-03-operators/src/challenge/main.py
+++ tutorials/30-days-of-code/day-03-operators/src/challenge/main.py
@@ -1,24 +1,26 @@
-#!/bin/python3
+#!/usr/bin/env python3
+"""
+HackerRank - 30 Days of Code - Day 03 - Operators
+"""
 
-import math
-import os
-import random
-import re
-import sys
 
-#
-# Complete the 'solve' function below.
-#
-# The function accepts following parameters:
-#  1. DOUBLE meal_cost
-#  2. INTEGER tip_percent
-#  3. INTEGER tax_percent
-#
+def solve(meal_cost: float, tip_percent: int, tax_percent: int):
+    """
+    Given the meal price (base cost of a meal), tip percent (the percentage of
+    the meal price being added as tip), and tax percent (the percentage of the
+    meal price being added as tax) for a meal, find and print the meal's total
+    cost. Round the result to the nearest integer.
+    """
 
-def solve(meal_cost, tip_percent, tax_percent):
-    # Write your code here
+    tip: float = meal_cost * (tip_percent / 100)
+    tax: float = meal_cost * (tax_percent / 100)
+    total: float = tip + tax + meal_cost
 
-if __name__ == '__main__':
+    # For the challange, solve() prints the total as an integer.
+    print(int(round(total, ndigits=0)))
+
+
+if __name__ == "__main__":
     meal_cost = float(input().strip())
 
     tip_percent = int(input().strip())
