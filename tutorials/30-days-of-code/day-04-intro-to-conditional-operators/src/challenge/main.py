7576a70f0f235048f411d45f57cf93b925714932
Victor Payno <vpayno@users.noreply.github.com>
2022-03-17 23:26:44 -0700

    30-days-of-code/day-04-intro-to-conditional-operators: 2nd iteration - added tests and typing

--- tutorials/30-days-of-code/day-04-intro-to-conditional-operators/src/challenge/main.py
+++ tutorials/30-days-of-code/day-04-intro-to-conditional-operators/src/challenge/main.py
@@ -1,12 +1,55 @@
-#!/bin/python3
+#!/usr/bin/env python3
+"""
+HackerRank - Tutorials - 30 Days of Code - Day 04 - Intro to Conditional Statements
 
-import math
-import os
-import random
-import re
-import sys
-
+Author: Victor Payno (https://github.com/vpayno/hackerrank-workspace)
+"""
 
 
-if __name__ == '__main__':
-    N = int(input().strip())
+class Challenge:
+    """
+    Day 04 - Intro to Conditional Statements
+    """
+
+    def __init__(self):
+
+        self.number: int = 0
+        self.result: str = ""
+
+    def input_number(self):
+        """
+        Read a string without a prompt to keep things interesting.
+        """
+        self.number = int(input().strip())
+
+    def solve(self):
+        """
+        Calculates if the number is weird or not.
+        """
+
+        if (self.number % 2) == 1:
+            self.result = "Weird"
+        elif (self.number % 2) == 0:
+            if self.number in range(2, 5 + 1):
+                self.result = "Not Weird"
+            elif self.number in range(6, 20 + 1):
+                self.result = "Weird"
+            elif self.number > 20:
+                self.result = "Not Weird"
+
+        print(self.result)
+
+    def main(self):
+        """
+        Read and save an integer, double, and String to your variables.
+        """
+        self.input_number()
+
+        self.solve()
+
+
+if __name__ == "__main__":  # pragma: no cover
+
+    challenge = Challenge()
+
+    challenge.main()
---
+++ tutorials/30-days-of-code/day-04-intro-to-conditional-operators/src/test/test_main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+Author: Victor Payno (https://github.com/vpayno)
+
+Challenge Tests
+"""
+
+import builtins
+import os.path
+import subprocess
+import sys
+
+import mock
+import pytest
+
+# Our Project
+from challenge import main
+
+unit_test_data = [
+    [3, "Weird"],
+    [4, "Not Weird"],
+    [7, "Weird"],
+    [8, "Weird"],
+    [21, "Weird"],
+    [22, "Not Weird"],
+]
+
+integration_test_data = [
+    ["3", "Weird"],
+    ["4", "Not Weird"],
+    ["7", "Weird"],
+    ["8", "Weird"],
+    ["21", "Weird"],
+    ["22", "Not Weird"],
+]
+
+
+@pytest.mark.parametrize("number,expected", unit_test_data)
+def test_method_with_input(number: int, expected: str, capsys):
+    """Runs the main class method against all of our test data."""
+
+    code: main.Challenge = main.Challenge()
+
+    with mock.patch.object(builtins, "input", lambda: str(number)):
+        code.input_number()
+
+    captured = capsys.readouterr()  # discard previous output
+    code.solve()
+    captured = capsys.readouterr()  # capture code.solve()
+
+    assert code.number == number
+    assert code.result == expected
+
+    expected_out: str
+    captured_out: str
+
+    expected_out = f"{expected}"
+
+    captured_out = captured.out.strip()
+
+    print(f"{captured_out} == {expected_out}")
+
+    assert captured_out == expected_out
+
+
+@pytest.mark.parametrize("number,expected", integration_test_data)
+def test_script(number: str, expected: str):
+    """Runs the main script against all of our test data."""
+
+    program_input: bytes = bytes(f"{number}", "utf8")
+
+    process: subprocess = subprocess.run(
+        [
+            sys.executable,
+            os.path.join(os.path.dirname("src/challenge/"), "main.py"),
+        ],
+        check=False,
+        input=program_input,
+        stdout=subprocess.PIPE,
+    )
+
+    program_output: str = process.stdout.decode("utf-8").strip()
+
+    print(f"  number = {number}")
+    print(f"expected = {expected}")
+    print(f"   input = {program_input}")
+    print(f"  output = {program_output}")
+    print(f"{program_output} == {expected}")
+
+    assert program_output == expected
