b67eaa7634a3620a857a5e7328c3cf9c427bb983
Victor Payno <vpayno@users.noreply.github.com>
2022-03-20 11:02:17 -0700

    prep-kit-3-months/week01/02-mini_max_sum: 1st iteration

--- prep-kit-3-months/week01/02-mini_max_sum/src/challenge/main.py
+++ prep-kit-3-months/week01/02-mini_max_sum/src/challenge/main.py
@@ -5,6 +5,7 @@ import os
 import random
 import re
 import sys
+from typing import List, Tuple
 
 #
 # Complete the 'miniMaxSum' function below.
@@ -12,11 +13,27 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def miniMaxSum(arr):
+
+def miniMaxSum(arr: List[int]):
     # Write your code here
 
-if __name__ == '__main__':
+    numbers: List[int] = sorted(arr)
+    slice_size: int = len(numbers) - 1
 
-    arr = list(map(int, input().rstrip().split()))
+    slice_min: List[int] = numbers[0:slice_size]
+    slice_max: List[int] = numbers[-slice_size:]
+
+    min: int = sum(slice_min)
+    max: int = sum(slice_max)
+
+    number: int
+    results: Tuple[int] = (min, max)
+
+    print(" ".join([str(number) for number in results]))
+
+
+if __name__ == "__main__":
+
+    arr: List[int] = list(map(int, input().rstrip().split()))
 
     miniMaxSum(arr)
