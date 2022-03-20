88ff2c656972b41d8c1cba99c3ef0b9d5dbd16c9
Victor Payno <vpayno@users.noreply.github.com>
2022-03-19 20:28:06 -0700

    30-days-of-code/day-04-class-vs-instance: 2nd iteration - tests, typing

--- tutorials/30-days-of-code/day-04-class-vs-instance/src/challenge/main.py
+++ tutorials/30-days-of-code/day-04-class-vs-instance/src/challenge/main.py
@@ -1,15 +1,32 @@
-class Person:
+#!/usr/bin/env python3
+"""
+HackerRank - Tutorials - 30 Days of Code - Day 04 - Class vs Instance
 
-    def __init__(self, initialAge):
-        # Add some more code to run some checks on initialAge
-        if initialAge >= 0:
-            self.age: int = initialAge
+Author: Victor Payno (https://github.com/vpayno/hackerrank-workspace)
+"""
+
+
+class Person:
+    """
+    Person with an age.
+    """
+
+    def __init__(self, age):
+
+        # The problem description says the age can be -5 <= age <= 30; But,
+        # that's not tested in the 3 test cases and it gets reset to 0 here if
+        # it's negative.
+        if age >= 0:
+            self.age: int = age
         else:
             print("Age is not valid, setting age to 0.")
             self.age = 0
 
-    def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
+    def am_i_old(self):
+        """
+        Checks the person's age to determine how old they are.
+        """
+
         if self.age < 13:
             print("You are young.")
         elif self.age >= 13 and self.age < 18:
@@ -17,19 +34,68 @@ class Person:
         else:
             print("You are old.")
 
-    def yearPasses(self):
-        # Increment the age of the person in her
+    def year_passes(self):
+        """
+        Increment the person's age by one.
+        """
+
         self.age += 1
 
 
-t = int(input())
-for i in range(0, t):
-    age = int(input())
-    p = Person(age)
-    p.amIOld()
+class Challenge:
+    """
+    Day 04 - Class vs Instance
+    """
 
-    for j in range(0, 3):
-        p.yearPasses()
+    def __init__(self):
 
-    p.amIOld()
-    print("")
+        self.age: int = 0
+        self.test_cases: int = 0
+
+    def input_test_cases(self):
+        """
+        Read an int without a prompt to keep things interesting.
+        """
+
+        # The problem page says the number of test cases is contrainted to
+        # 1 <= T < 4 but it's not enforced in the stub code they provide.
+        self.test_cases = int(input().strip())
+
+    def input_age(self):
+        """
+        Read an int without a prompt to keep things interesting.
+        """
+
+        self.age = int(input().strip())
+
+    def solve(self):
+        """
+        Calculates if the user is old.
+        """
+
+        for _ in range(0, self.test_cases):
+            self.input_age()
+            person: Person = Person(self.age)
+            person.am_i_old()
+
+            for _ in range(0, 3):
+                person.year_passes()
+
+            person.am_i_old()
+            print("")
+
+    def main(self):
+        """
+        Challenge steps.
+        """
+
+        self.input_test_cases()
+
+        self.solve()
+
+
+if __name__ == "__main__":  # pragma: no cover
+
+    challenge = Challenge()
+
+    challenge.main()
---
+++ tutorials/30-days-of-code/day-04-class-vs-instance/src/test/test_main.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+"""
+Author: Victor Payno (https://github.com/vpayno)
+
+Challenge Tests
+"""
+
+import builtins
+import os.path
+import subprocess
+import sys
+from typing import List
+
+import mock
+import pytest
+
+# Our Project
+from challenge import main
+
+unit_test_data_person = [
+    [-1, "Age is not valid, setting age to 0.\nYou are young."],
+    [0, "You are young."],
+    [7, "You are young."],
+    [11, "You are young."],
+    [12, "You are young."],
+    [13, "You are a teenager."],
+    [14, "You are a teenager."],
+    [17, "You are a teenager."],
+    [18, "You are old."],
+    [19, "You are old."],
+    [29, "You are old."],
+]
+
+unit_test_data = [
+    [1, -1, "Age is not valid, setting age to 0.\nYou are young."],
+    [1, 0, "You are young."],
+    [1, 7, "You are young."],
+    [1, 11, "You are young."],
+    [1, 12, "You are young."],
+    [1, 13, "You are a teenager."],
+    [1, 14, "You are a teenager."],
+    [1, 17, "You are a teenager."],
+    [1, 18, "You are old."],
+    [1, 19, "You are old."],
+    [1, 29, "You are old."],
+]
+
+integration_test_data = [[
+    4,
+    [-1, 10, 16, 18],
+    [
+        "Age is not valid, setting age to 0.",
+        "You are young.",
+        "You are young.",
+        "",
+        "You are young.",
+        "You are a teenager.",
+        "",
+        "You are a teenager.",
+        "You are old.",
+        "",
+        "You are old.",
+        "You are old.",
+        "",
+    ],
+]]
+
+
+@pytest.mark.parametrize("age,expected", unit_test_data_person)
+def test_class_person(age: int, expected: str, capsys):
+    """Runs the class methods against all of our test data."""
+
+    captured = capsys.readouterr()  # discard previous output
+
+    code: main.Person = main.Person(age)
+
+    expected_out: List[str]
+    captured_out: List[str]
+
+    code.am_i_old()
+    captured = capsys.readouterr()  # capture new output
+
+    captured_out = captured.out.strip().split("\n")
+    expected_out = expected.split("\n")
+
+    print(f"{captured_out} == {expected_out}")
+    assert all(e == o for e, o in zip(captured_out, expected_out))
+
+    age = max(age, 0)
+
+    assert code.age == age
+
+    for _ in range(0, 5):
+        code.year_passes()
+        age += 1
+        assert code.age == age
+
+
+@pytest.mark.parametrize("test_cases,age,expected", unit_test_data)
+def test_method_with_input(test_cases: int, age: int, expected: str, capsys):
+    """Runs the class method against all of our test data."""
+
+    captured = capsys.readouterr()  # discard previous output
+
+    code: main.Challenge = main.Challenge()
+    person: main.Person = main.Person(age)
+
+    expected_out: List[str]
+    captured_out: List[str]
+
+    with mock.patch.object(builtins, "input", lambda: str(test_cases)):
+        code.input_test_cases()
+
+    with mock.patch.object(builtins, "input", lambda: str(age)):
+        code.input_age()
+
+    person.am_i_old()
+    captured = capsys.readouterr()  # capture new output
+
+    captured_out = captured.out.strip().split("\n")
+    expected_out = expected.split("\n")
+
+    print(f"{captured_out} == {expected_out}")
+    assert all(e == o for e, o in zip(captured_out, expected_out))
+
+    assert code.test_cases == test_cases
+
+    assert code.age == age
+
+
+@pytest.mark.parametrize("test_cases,ages,expected", integration_test_data)
+def test_script(test_cases: int, ages: List[int], expected: List[str]):
+    """Runs the main script against all of our test data."""
+
+    program_input: bytes = bytes(
+        f"{test_cases}\n" + "\n".join([str(number) for number in ages]), "utf8")
+
+    process: subprocess.CompletedProcess = subprocess.run(
+        [
+            sys.executable,
+            os.path.join(os.path.dirname("src/challenge/"), "main.py"),
+        ],
+        check=False,
+        input=program_input,
+        stdout=subprocess.PIPE,
+    )
+
+    program_output: str = process.stdout.decode("utf-8").strip()
+
+    print(f"test_cases = {test_cases}")
+    print("ages = " + ",".join([str(number) for number in ages]))
+    print(f"expected = {expected}")
+    print(f"   input = {program_input!r}")
+    print(f"  output = {program_output}")
+    print(f"{program_output} == {expected}")
+
+    assert all(e == o for e, o in zip(program_output.split("\n"), expected))
