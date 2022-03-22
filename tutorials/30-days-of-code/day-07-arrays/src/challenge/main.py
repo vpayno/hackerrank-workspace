54747525236194dbe3b306891bea5fd352ce861d
Victor Payno <vpayno@users.noreply.github.com>
2022-03-21 22:45:05 -0700

    30-days-of-code/day-07-arrays: download challenge

---
+++ tutorials/30-days-of-code/day-07-arrays/src/challenge/main.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from typing import List
+
+if __name__ == "__main__":
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
