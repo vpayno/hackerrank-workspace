dc53044d1684c95372846269c1a87739164685f1
Victor Payno <vpayno@users.noreply.github.com>
2022-03-21 22:46:05 -0700

    30-days-of-code/day-07-arrays: 1st iteration

--- tutorials/30-days-of-code/day-07-arrays/src/challenge/main.py
+++ tutorials/30-days-of-code/day-07-arrays/src/challenge/main.py
@@ -1,13 +1,15 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
 from typing import List
+
+
+def solve(numbers: List[int]):
+    print(" ".join([str(number) for number in reversed(numbers)]))
+
 
 if __name__ == "__main__":
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
+
+    solve(arr)
