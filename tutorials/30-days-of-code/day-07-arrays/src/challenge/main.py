94733ff83c06d075c7b9de068fe90e6938484351
Victor Payno <vpayno@users.noreply.github.com>
2022-03-23 21:42:52 -0700

    30-days-of-code/day-07-arrays: 2nd iteration - tests, typing

--- tutorials/30-days-of-code/day-07-arrays/src/challenge/main.py
+++ tutorials/30-days-of-code/day-07-arrays/src/challenge/main.py
@@ -1,15 +1,131 @@
-#!/bin/python3
+#!/usr/bin/env python3
+"""
+HackerRank - Tutorials - 30 Days of Code - Day 07 - Arrays
 
-from typing import List
+Author: Victor Payno (https://github.com/vpayno/hackerrank-workspace)
+"""
+
+from typing import List, Optional
+
+from rich.traceback import install
+
+install()
 
 
-def solve(numbers: List[int]):
-    print(" ".join([str(number) for number in reversed(numbers)]))
+class InputDoesNotMatchQuantityException(Exception):
+    """Raised when the quantity input doesn't match the number of numbers in
+    the 2nd input.
+
+    Attributes:
+        quantity -- expected number of numbers in 2nd input
+        numbers -- list of numbers from 2nd input
+    """
+
+    def __init__(
+        self,
+        quantity: int,
+        numbers: List[int],
+        message:
+        str = "Quantity of numbers in 2nd input doesn't match the quantity " +
+        "described in the 1st input.",
+    ):
+
+        self.quantity: int = quantity
+        self.numbers: List[int] = numbers
+        self.message: str = message
+
+        super().__init__(self.message)
+
+    def __str__(self):
+        return (
+            f"quantity={self.quantity} != len(numbers)={len(self.numbers)}\n" +
+            f"{self.message}")
 
 
-if __name__ == "__main__":
-    n = int(input().strip())
+class Challenge:
+    """
+    Day 07 - Arrays
+    """
 
-    arr = list(map(int, input().rstrip().split()))
+    def __init__(self, numbers: Optional[List[int]] = None):
 
-    solve(arr)
+        self.numbers: List[int] = []
+        self.quantity: int = 0
+
+        self.output: List[int] = []
+
+        if numbers is not None:
+            # mypy: Argument 1 to "append" of "list" has incompatible type
+            # "List[str]"; expected "str"
+            # self.numbers.append(numbers)
+            for line in numbers:
+                self.numbers.append(line)
+            self.quantity = len(numbers)
+        else:
+            self.numbers = []
+
+    def input_quantity(self):
+        """
+        Read an int without a prompt to keep things interesting.
+        """
+
+        if self.quantity <= 0:
+            self.quantity = max(int(input().strip()), 0)
+        else:
+            pass
+
+    def input_numbers(self):
+        """
+        Read a space separated list of numbers without a prompt to keep things
+        interesting.
+        """
+
+        if len(self.numbers) <= 0:
+            self.numbers = [
+                int(number) for number in input().strip().split(" ")
+            ]
+        else:
+            pass
+
+        # We're not using the first integer but maybe we should through an
+        # exception if the quantity number doesn't match the number of numbers
+        # in the second inputs.
+        if self.quantity == len(self.numbers):
+            # We need if/else for coverage but pylint doesn't like an else after a raise.
+            pass
+        else:
+            raise InputDoesNotMatchQuantityException(self.quantity,
+                                                     self.numbers)
+
+    def solve(self):
+        """
+        Reverse the numbers in the list.
+        """
+
+        self.output = list(reversed(self.numbers))
+
+    def print_results(self):
+        """Print the results of the challenge."""
+
+        print(" ".join([str(number) for number in self.output]))
+
+    def main(self):
+        """
+        Challenge steps.
+        """
+
+        self.input_quantity()
+
+        # Intentionally not catching the raised exception.
+        self.input_numbers()
+
+        self.solve()
+
+        self.print_results()
+
+
+if __name__ == "__main__":  # pragma: no cover
+
+    challenge = Challenge()
+
+    challenge.main()
---
+++ tutorials/30-days-of-code/day-07-arrays/src/test/test_main.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+"""
+Author: Victor Payno (https://github.com/vpayno)
+
+Challenge Tests
+"""
+
+import builtins
+import os.path
+import subprocess
+import sys
+from typing import List
+from unittest.mock import patch
+
+import mock
+import pytest
+
+# Our Project
+from challenge import main
+
+unit_test_data = [
+    [4, [1, 4, 3, 2], [2, 3, 4, 1]],
+    [6, [1, -1, 3, 7, 4, 1], [1, 4, 7, 3, -1, 1]],
+]
+
+integration_test_data = unit_test_data
+
+
+def test_raise_exception():
+    """Test the custom exception."""
+    code: main.Challenge = main.Challenge()
+
+    code.quantity = 1
+    code.numbers = [1, 2, 3]
+
+    expected_out: List[str] = [
+        f"quantity={code.quantity} != len(numbers)={len(code.numbers)}",
+        "Quantity of numbers in 2nd input doesn't match the quantity described in the 1st input.",
+    ]
+
+    with pytest.raises(main.InputDoesNotMatchQuantityException) as excinfo:
+        # Won't call input since code.numbers is already set, we are just testing
+        # the if block after that.
+        code.input_numbers()
+
+    captured_out: List[str] = str(excinfo.value).split("\n")
+
+    print(f"{captured_out} == {expected_out}")
+    assert all(e == o for e, o in zip(captured_out, expected_out))
+
+
+@pytest.mark.parametrize("quantity,numbers,expected", unit_test_data)
+def test_method_without_input(quantity: int, numbers: List[int],
+                              expected: List[int], capsys):
+    """Runs the class methods against all of our test data."""
+
+    captured_out: List[int]
+    expected_out: List[int]
+
+    code: main.Challenge = main.Challenge(numbers)
+
+    with mock.patch.object(builtins, "input", lambda: str(quantity)):
+        code.input_quantity()
+
+    assert code.quantity == max(quantity, 0)
+
+    with patch("builtins.input") as mock_input:
+        mock_input.side_effect = [" ".join([str(number) for number in numbers])]
+        code.input_numbers()
+
+    assert mock_input.call_count == 0
+    assert len(code.numbers) == len(numbers)
+
+    code.solve()
+
+    # captured = capsys.readouterr()  # discard previous output
+    code.print_results()
+    captured = capsys.readouterr()  # capture new output
+
+    captured_out = [int(number) for number in captured.out.strip().split(" ")]
+    expected_out = expected
+
+    print(f"{captured_out} == {expected_out}")
+    assert all(e == o for e, o in zip(captured_out, expected_out))
+
+
+@pytest.mark.parametrize("quantity,numbers,expected", unit_test_data)
+def test_method_with_input(quantity: int, numbers: List[int],
+                           expected: List[int], capsys):
+    """Runs the class method against all of our test data."""
+
+    captured_out: List[int]
+    expected_out: List[int]
+
+    code: main.Challenge = main.Challenge()
+
+    with mock.patch.object(builtins, "input", lambda: str(quantity)):
+        code.input_quantity()
+
+    with patch("builtins.input") as mock_input:
+        mock_input.side_effect = [" ".join([str(number) for number in numbers])]
+        code.input_numbers()
+
+    assert mock_input.call_count == 1
+    assert code.quantity == max(quantity, 0)
+
+    code.solve()
+
+    # captured = capsys.readouterr()  # discard previous output
+    code.print_results()
+    captured = capsys.readouterr()  # capture new output
+
+    captured_out = [int(number) for number in captured.out.strip().split(" ")]
+    expected_out = expected
+
+    print(f"code.output == {code.output}")
+    print(f"{captured_out} == {expected_out}")
+    assert all(e == o for e, o in zip(captured_out, expected_out))
+
+
+@pytest.mark.parametrize("quantity,numbers,expected", unit_test_data)
+def test_script(quantity: int, numbers: List[int], expected: List[int]):
+    """Runs the main script against all of our test data."""
+
+    program_input: bytes = bytes(f"{quantity}\n", "utf8")
+    program_input += bytes(" ".join([str(number) for number in numbers]),
+                           "utf8")
+
+    process: subprocess.CompletedProcess = subprocess.run(
+        [
+            sys.executable,
+            os.path.join(os.path.dirname("src/challenge/"), "main.py"),
+        ],
+        check=False,
+        input=program_input,
+        stdout=subprocess.PIPE,
+    )
+
+    program_output: str = process.stdout.decode("utf-8").strip()
+    program_out: List[int] = [
+        int(number) for number in program_output.split(" ")
+    ]
+
+    print(f"   quantity = {quantity}")
+    print(f"    numbers = {numbers}")
+    print(f"   expected = {expected}")
+    print(f"      input = {program_input!r}")
+    print(f"     output = {program_output}")
+    print(f"{program_output} == {expected}")
+
+    assert all(e == o for e, o in zip(program_out, expected))
