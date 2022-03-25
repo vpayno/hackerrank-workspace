9473f368e213cf4cf1d9d3f1a8cfc0c5495d638f
Victor Payno <vpayno@users.noreply.github.com>
2022-03-24 21:36:41 -0700

    30-days-of-code/day-10-binary_numbers: 1st iteration

--- tutorials/30-days-of-code/day-10-binary_numbers/src/challenge/main.py
+++ tutorials/30-days-of-code/day-10-binary_numbers/src/challenge/main.py
@@ -7,6 +7,28 @@ import re
 import sys
 
 
+def solve(decimal: int):
 
-if __name__ == '__main__':
-    n = int(input().strip()
+    binary: bin = bin(decimal)
+    char: str
+    count: int = 0
+    counts: set = set()
+
+    for char in str(binary)[2:]:
+        if char == "1":
+            count += 1
+        else:
+            counts.add(count)
+            count = 0
+
+    counts.add(count)
+
+    # print(binary)
+    # print(counts)
+    print(sorted(list(counts))[-1])
+
+
+if __name__ == "__main__":
+    n: int = int(input().strip())
+
+    solve(n)
