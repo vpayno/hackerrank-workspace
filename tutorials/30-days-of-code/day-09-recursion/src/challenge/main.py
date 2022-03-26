1ebd3f97b9b6a6fb4ef0ddcf927c5dfd48ba10b9
Victor Payno <vpayno@users.noreply.github.com>
2022-03-25 19:42:41 -0700

    30-days-of-code/day-09-recursion: 2nd iteration - typing, tests

---
+++ tutorials/30-days-of-code/day-09-recursion/src/challenge/main.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""
+HackerRank - Tutorials - 30 Days of Code - Day 09 - Recursion 3
+
+Author: Victor Payno (https://github.com/vpayno/hackerrank-workspace)
+"""
+
+import os
+import sys
+from typing import Optional
+
+from rich.traceback import install
+
+install()
+
+
+class Challenge:
+    """
+    Day 07 - Arrays
+    """
+
+    def __init__(self, number: Optional[int] = None):
+
+        self.number: int = 0
+
+        self.output: int = 0
+
+        if number is not None:
+            self.number = number
+        else:
+            self.numbers = 0
+
+    def input_number(self):
+        """
+        Read an int without a prompt to keep things interesting.
+        """
+
+        if self.number <= 0:
+            self.number = max(int(input().strip()), 0)
+        else:
+            pass
+
+    def factorial(self, number: int) -> int:
+        """Recursive factorial function."""
+
+        retval: int
+
+        if number > 1:
+            retval = number * self.factorial(number - 1)
+        else:
+            retval = number
+
+        return retval
+
+    def solve(self):
+        """Solves the challenge."""
+
+        self.output = self.factorial(self.number)
+
+    def print_results(self):
+        """Print the results of the challenge."""
+
+        if "pytest" in sys.modules:
+
+            print(self.output)
+
+        else:
+
+            try:
+                fptr = open(os.environ["OUTPUT_PATH"], "w", encoding="utf8")
+            except KeyError:
+                fptr = sys.stdout
+
+            with fptr:
+                fptr.write(str(self.output) + "\n")
+
+                fptr.close()
+
+    def main(self):
+        """
+        Challenge steps.
+        """
+
+        self.input_number()
+
+        self.solve()
+
+        self.print_results()
+
+
+if __name__ == "__main__":  # pragma: no cover
+
+    challenge = Challenge()
+
+    challenge.main()
