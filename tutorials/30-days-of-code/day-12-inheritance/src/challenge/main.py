84fbb94a753f3d334f15238b7007723df398f5c9
Victor Payno <vpayno@users.noreply.github.com>
2022-03-26 22:59:02 -0700

    30-days-of-code/day-12-inheritance: 1st iteration

--- tutorials/30-days-of-code/day-12-inheritance/src/challenge/main.py
+++ tutorials/30-days-of-code/day-12-inheritance/src/challenge/main.py
@@ -1,35 +1,66 @@
+#!/usr/bin/env python3
+"""
+Day 12 - Inheritance
+"""
+
+from typing import List
+
+
 class Person:
-	def __init__(self, firstName, lastName, idNumber):
-		self.firstName = firstName
-		self.lastName = lastName
-		self.idNumber = idNumber
-	def printPerson(self):
-		print("Name:", self.lastName + ",", self.firstName)
-		print("ID:", self.idNumber)
+    """Person class"""
+
+    def __init__(self, firstName: str, lastName: str, idNumber: str):
+
+        self.firstName: str = firstName
+        self.lastName: str = lastName
+        self.idNumber: str = idNumber
+
+    def printPerson(self):
+        """How info about a person."""
+
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
 
 class Student(Person):
-    #   Class Constructor
-    #   
-    #   Parameters:
-    #   firstName - A string denoting the Person's first name.
-    #   lastName - A string denoting the Person's last name.
-    #   id - An integer denoting the Person's ID number.
-    #   scores - An array of integers denoting the Person's test scores.
-    #
-    # Write your constructor here
-    
+    """Inherits Person, adds scores and calculate()"""
 
-    #   Function Name: calculate
-    #   Return: A character denoting the grade.
-    #
-    # Write your function here
+    def __init__(self, firstName: str, lastName: str, idNumber: str,
+                 scores: List[int]):
 
-line = input().split()
-firstName = line[0]
-lastName = line[1]
-idNum = line[2]
-numScores = int(input()) # not needed for Python
-scores = list( map(int, input().split()) )
-s = Student(firstName, lastName, idNum, scores)
+        super().__init__(firstName, lastName, idNumber)
+
+        self.scores: List[int] = scores.copy()
+
+    def calculate(self) -> str:
+        """Calculate a student's average and return the grade."""
+        average: float
+        grade: str
+
+        average = sum(self.scores) / len(self.scores)
+
+        if average >= 90:
+            grade = "O"
+        elif average >= 80:
+            grade = "E"
+        elif average >= 70:
+            grade = "A"
+        elif average >= 55:
+            grade = "P"
+        elif average >= 40:
+            grade = "D"
+        else:
+            grade = "T"
+
+        return grade
+
+
+line: List[str] = input().split()
+firstName: str = line[0]
+lastName: str = line[1]
+idNum: str = line[2]
+numScores: int = int(input())  # not needed for Python
+scores: List[int] = list(map(int, input().split()))
+s: Student = Student(firstName, lastName, idNum, scores)
 s.printPerson()
 print("Grade:", s.calculate())
