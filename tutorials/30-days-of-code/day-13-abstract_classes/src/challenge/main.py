2e704d0b08a7179973aaed7d8d30ba493147fa07
Victor Payno <vpayno@users.noreply.github.com>
2022-03-27 14:19:29 -0700

    30-days-of-code/day-13-abstract_classes: 1st iteration

--- tutorials/30-days-of-code/day-13-abstract_classes/src/challenge/main.py
+++ tutorials/30-days-of-code/day-13-abstract_classes/src/challenge/main.py
@@ -1,15 +1,34 @@
+#!/usr/bin/env python3
+
 from abc import ABCMeta, abstractmethod
+
+
 class Book(object, metaclass=ABCMeta):
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author   
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @staticmethod
     @abstractmethod
-    def display(): pass
+    def display():
+        pass
 
-#Write MyBook class
 
-title=input()
-author=input()
-price=int(input())
-new_novel=MyBook(title,author,price)
+class MyBook(Book):
+
+    def __init__(self, title: str, author: str, price: int):
+        super().__init__(title, author)
+        self.price: int = price
+
+    def display(self):
+        print(f"Title: {title}")
+        print(f"Author: {author}")
+        print(f"Price: {price}")
+
+
+title = input()
+author = input()
+price = int(input())
+new_novel = MyBook(title, author, price)
 new_novel.display()
