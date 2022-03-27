fcecd9c107d0e2780051391e78e75640ea69204e
Victor Payno <vpayno@users.noreply.github.com>
2022-03-27 15:49:19 -0700

    30-days-of-code/day-13-abstract_classes: 2nd iteration - typing, tests

--- tutorials/30-days-of-code/day-13-abstract_classes/src/challenge/main.py
+++ tutorials/30-days-of-code/day-13-abstract_classes/src/challenge/main.py
@@ -1,34 +1,109 @@
 #!/usr/bin/env python3
+"""
+HackerRank - Tutorials - 30 Days of Code - Day 10 - Binary Numbers
+
+Author: Victor Payno (https://github.com/vpayno/hackerrank-workspace)
+"""
 
 from abc import ABCMeta, abstractmethod
+from typing import Optional
+
+from rich.traceback import install
+
+install()
 
 
-class Book(object, metaclass=ABCMeta):
+class Book(metaclass=ABCMeta):
+    """Book class consisting of a title and author."""
 
     def __init__(self, title, author):
+
         self.title = title
         self.author = author
 
     @staticmethod
     @abstractmethod
+    def __str__() -> str:
+        """Abstract method."""
+
+    @staticmethod
+    @abstractmethod
     def display():
-        pass
+        """Abstract method."""
 
 
 class MyBook(Book):
+    """MyBook extends Book and adds price."""
 
     def __init__(self, title: str, author: str, price: int):
+
         super().__init__(title, author)
         self.price: int = price
 
+    def __str__(self) -> str:
+        """Implements book.__str__()."""
+
+        return (f"Title: {self.title}\n" + f"Author: {self.author}\n" +
+                f"Price: {self.price}\n")
+
     def display(self):
-        print(f"Title: {title}")
-        print(f"Author: {author}")
-        print(f"Price: {price}")
+        """Implements book.display()."""
+
+        print(self)
 
 
-title = input()
-author = input()
-price = int(input())
-new_novel = MyBook(title, author, price)
-new_novel.display()
+class Challenge:
+    """
+    Main challenge class.
+    """
+
+    def __init__(self, mybook: Optional[MyBook] = None):
+
+        self.mybook: Optional[MyBook]
+
+        if mybook is not None:
+            self.mybook = mybook
+        else:
+            self.mybook = None
+
+    def input_book(self):
+        """
+        Read inputs without a prompt to keep things interesting.
+        """
+
+        if self.mybook is None or None in [
+                self.mybook.title,
+                self.mybook.author,
+                self.mybook.price,
+        ]:
+            title: str = input().strip()
+            author: str = input().strip()
+            price = int(input().strip())
+
+            self.mybook = MyBook(title, author, price)
+        else:
+            pass
+
+    def print_results(self):
+        """Print the results of the challenge."""
+
+        if self.mybook is not None:
+            self.mybook.display()
+        else:
+            pass
+
+    def main(self):
+        """
+        Challenge steps.
+        """
+
+        self.input_book()
+
+        self.print_results()
+
+
+if __name__ == "__main__":  # pragma: no cover
+
+    challenge = Challenge()
+
+    challenge.main()
---
+++ tutorials/30-days-of-code/day-13-abstract_classes/src/test/test_main.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+"""
+Author: Victor Payno (https://github.com/vpayno)
+
+Challenge Tests
+"""
+
+import os.path
+import subprocess
+import sys
+from typing import List
+from unittest.mock import patch
+
+import pytest
+
+# Our Project
+from challenge import main
+from challenge.main import MyBook
+
+unit_test_data = [
+    [
+        ["The Alchemist", "Paulo Coelho", "248"],
+        ["Title: The Alchemist", "Author: Paulo Coelho", "Price: 248"],
+    ],
+    [
+        ["The Book", "First Last", "123"],
+        ["Title: The Book", "Author: First Last", "Price: 123"],
+    ],
+]
+
+integration_test_data = unit_test_data
+
+
+@pytest.mark.parametrize("data,expected", unit_test_data)
+def test_method_without_input(data: List[str], expected: List[str], capsys):
+    """Runs the class methods against all of our test data."""
+
+    captured_out: List[str]
+    expected_out: List[str]
+
+    title: str = data[0]
+    author: str = data[1]
+    price: int = int(data[2])
+
+    mybook: MyBook = MyBook(title, author, price)
+
+    code: main.Challenge = main.Challenge(mybook)
+
+    with patch("builtins.input") as mock_input:
+        mock_input.side_effect = data
+        code.input_book()
+
+    assert mybook.title == title
+    assert mybook.author == author
+    assert mybook.price == price
+
+    # captured = capsys.readouterr()  # discard previous output
+    code.print_results()
+    captured = capsys.readouterr()  # capture new output
+
+    captured_out = captured.out.strip().split("\n")
+    expected_out = expected
+
+    print(f"{captured_out} == {expected_out}")
+    assert all(e == o for e, o in zip(captured_out, expected_out))
+
+
+@pytest.mark.parametrize("data,expected", unit_test_data)
+def test_method_with_input(data: List[str], expected: List[str], capsys):
+    """Runs the class method against all of our test data."""
+
+    captured_out: List[str]
+    expected_out: List[str]
+
+    title: str = data[0]
+    author: str = data[1]
+    price: int = int(data[2])
+
+    mybook: MyBook = MyBook(title, author, price)
+
+    code: main.Challenge = main.Challenge()
+
+    # shouldn't do anything (testing else:pass)
+    code.print_results()
+
+    with patch("builtins.input") as mock_input:
+        mock_input.side_effect = data
+        code.input_book()
+
+    assert mybook.title == title
+    assert mybook.author == author
+    assert mybook.price == price
+
+    # captured = capsys.readouterr()  # discard previous output
+    code.print_results()
+    captured = capsys.readouterr()  # capture new output
+
+    captured_out = captured.out.strip().split("\n")
+    expected_out = expected
+
+    print(f"{captured_out} == {expected_out}")
+    assert all(e == o for e, o in zip(captured_out, expected_out))
+
+
+@pytest.mark.parametrize("data,expected", unit_test_data)
+def test_script(data: List[str], expected: List[str]):
+    """Runs the main script against all of our test data."""
+
+    program_input: bytes = bytes("\n".join(data), "utf8")
+
+    process: subprocess.CompletedProcess = subprocess.run(
+        [
+            sys.executable,
+            os.path.join(os.path.dirname("src/challenge/"), "main.py"),
+        ],
+        check=False,
+        input=program_input,
+        stdout=subprocess.PIPE,
+    )
+
+    program_output: str = process.stdout.decode("utf-8").strip()
+    program_out: List[str] = program_output.split("\n")
+
+    expected_out = expected
+
+    print(f"       data = {data}")
+    print(f"   expected = {expected}")
+    print(f"      input = {program_input!r}")
+    print(f"     output = {program_output}")
+    print(f"{program_out} == {expected_out}")
+
+    assert all(e == o for e, o in zip(program_out, expected_out))
