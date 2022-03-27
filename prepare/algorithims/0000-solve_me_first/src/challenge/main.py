f8fe50e69313a7ee6b03009aa8b02562d5297c83
Victor Payno <vpayno@users.noreply.github.com>
2022-03-27 16:23:49 -0700

    algorithims/0000-solve_me_first: 1st iteration

--- prepare/algorithims/0000-solve_me_first/src/challenge/main.py
+++ prepare/algorithims/0000-solve_me_first/src/challenge/main.py
@@ -1,8 +1,16 @@
-def solveMeFirst(a, b):
-    # Hint: Type return a+b below
+#!/usr/bin/env python3
+"""
+Algorithims - Warmup - Solve Me First
+"""
 
 
-num1 = int(input())
-num2 = int(input())
-res = solveMeFirst(num1, num2)
+def solve_me_first(number1: int, number2: int) -> int:
+    """Hint: Type return a+b below"""
+
+    return number1 + number2
+
+
+num1: int = int(input())
+num2: int = int(input())
+res: int = solve_me_first(num1, num2)
 print(res)
