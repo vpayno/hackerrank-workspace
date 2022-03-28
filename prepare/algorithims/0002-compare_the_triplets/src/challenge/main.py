919e60ef87eed5950c970069460344bcfbf57c3b
Victor Payno <vpayno@users.noreply.github.com>
2022-03-27 17:54:44 -0700

    algorithims/0002-compare_the_triplets: 1st iteration

--- prepare/algorithims/0002-compare_the_triplets/src/challenge/main.py
+++ prepare/algorithims/0002-compare_the_triplets/src/challenge/main.py
@@ -17,10 +17,22 @@ import sys
 
 
 def compareTriplets(a, b):
+    score_alice: int = 0
+    score_bob: int = 0
+
+    for num1, num2 in zip(a, b):
+        if num1 > num2:
+            score_alice += 1
+        elif num1 < num2:
+            score_bob += 1
+        else:
+            pass
+
+    return [score_alice, score_bob]
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     a = list(map(int, input().rstrip().split()))
 
@@ -28,7 +40,7 @@ if __name__ == '__main__':
 
     result = compareTriplets(a, b)
 
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
+    fptr.write(" ".join(map(str, result)))
+    fptr.write("\n")
 
     fptr.close()
