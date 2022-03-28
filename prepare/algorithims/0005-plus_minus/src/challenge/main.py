3ed62002e74a1f82419bee558805c3cb347ca193
Victor Payno <vpayno@users.noreply.github.com>
2022-03-27 18:52:33 -0700

    algorithims/0005-plus_minus: 1st iteration

--- prepare/algorithims/0005-plus_minus/src/challenge/main.py
+++ prepare/algorithims/0005-plus_minus/src/challenge/main.py
@@ -1,10 +1,12 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
+import collections
 import math
 import os
 import random
 import re
 import sys
+from typing import List, OrderedDict
 
 #
 # Complete the 'plusMinus' function below.
@@ -12,13 +14,38 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def plusMinus(arr):
-    # Write your code here
 
-if __name__ == '__main__':
+def plusMinus(arr: List[int]):
+    counter: OrderedDict[str, int] = collections.OrderedDict({
+        "positive": 0,
+        "negative": 0,
+        "zero": 0,
+    })
+
+    size: float = len(arr)
+    num: int
+
+    for num in arr:
+        if num > 0:
+            counter["positive"] += 1
+        elif num < 0:
+            counter["negative"] += 1
+        else:
+            counter["zero"] += 1
+
+    result: List[float] = []
+    value: float
+
+    for _, value in counter.items():
+        result.append(value / size)
+
+    for value in result:
+        print(f"{value:.6f}")
+
+
+if __name__ == "__main__":
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
 
     plusMinus(arr)
-
