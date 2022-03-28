752f868133fa3ebcfb4f3a85c79283a6717c5d29
Victor Payno <vpayno@users.noreply.github.com>
2022-03-28 13:17:41 -0700

    30-days-of-code/day-13-abstract_classes: 1st iteration

--- tutorials/30-days-of-code/day-14-scope/src/challenge/main.py
+++ tutorials/30-days-of-code/day-14-scope/src/challenge/main.py
@@ -1,9 +1,21 @@
 class Difference:
 
-    def __init__(self, a):
-        self.__elements = a
+    def __init__(self, numbers):
+        self.__elements = numbers
+        self.deltas = set()
+        self.maximumDifference = 0
 
-        # Add your code here
+    def computeDifference(self):
+
+        offset = 1
+
+        for num1 in self.__elements[:-1]:
+            for num2 in self.__elements[offset:]:
+                diff = abs(num1 - num2)
+                self.deltas.add(diff)
+            offset += 1
+
+        self.maximumDifference = sorted(list(self.deltas))[-1]
 
 
 # End of Difference class
