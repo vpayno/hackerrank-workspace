4e91b18b2c0dc743807b8cee7f6aa0a48ec6e594
Victor Payno <vpayno@users.noreply.github.com>
2022-03-27 18:33:04 -0700

    algorithims/0004-diagonal_difference: 1st iteration

--- prepare/algorithims/0004-diagonal_difference/src/challenge/main.py
+++ prepare/algorithims/0004-diagonal_difference/src/challenge/main.py
@@ -1,10 +1,11 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
 import math
 import os
 import random
 import re
 import sys
+from typing import List
 
 #
 # Complete the 'diagonalDifference' function below.
@@ -13,11 +14,26 @@ import sys
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
 #
 
-def diagonalDifference(arr):
-    # Write your code here
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+def diagonalDifference(arr: List[List[int]]) -> int:
+
+    total1: int = 0
+    total2: int = 0
+    diff: int
+
+    for index1, index2 in zip(range(0, len(arr)), range(0, len(arr), 1)):
+        total1 += arr[index1][index2]
+
+    for index1, index2 in zip(range(0, len(arr)), range(len(arr) - 1, -1, -1)):
+        total2 += arr[index1][index2]
+
+    diff = abs(total1 - total2)
+
+    return diff
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     n = int(input().strip())
 
@@ -28,7 +44,6 @@ if __name__ == '__main__':
 
     result = diagonalDifference(arr)
 
-    fptr.write(str(result) + '\n')
+    fptr.write(str(result) + "\n")
 
     fptr.close()
-
