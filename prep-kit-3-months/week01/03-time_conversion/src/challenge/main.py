9d8be07927489c35a3ee052c6831701aa6681e74
Victor Payno <vpayno@users.noreply.github.com>
2022-03-28 20:27:33 -0700

    prep-kit-3-months/week01/03-time_conversion: 1st iteration

--- prep-kit-3-months/week01/03-time_conversion/src/challenge/main.py
+++ prep-kit-3-months/week01/03-time_conversion/src/challenge/main.py
@@ -6,23 +6,45 @@ import random
 import re
 import sys
 
-#
-# Complete the 'timeConversion' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING s as parameter.
-#
 
-def timeConversion(s):
-    # Write your code here
+def timeConversion(s: str) -> str:
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    hour: int
+    minute: int
+    second: int
+    abbv: str
+
+    morning: bool
+
+    m: re.Match = re.match(r"(\d{2}):(\d{2}):(\d{2})(AM|PM)", s)
+
+    hour, minute, second, abbv = m.groups()
+
+    morning = abbv == "AM"
+
+    if morning:
+        if int(hour) == 12:
+            hour = "00"
+        else:
+            pass
+    else:
+        if int(hour) == 12:
+            pass
+        else:
+            hour = str(int(hour) + 12)
+
+    new_time = f"{hour}:{minute}:{second}"
+
+    return new_time
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     s = input()
 
     result = timeConversion(s)
 
-    fptr.write(result + '\n')
+    fptr.write(result + "\n")
 
     fptr.close()
