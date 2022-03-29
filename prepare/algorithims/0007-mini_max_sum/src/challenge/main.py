6ba0f4d2c6b83eb8d03c7b568e4e69934a54f1dc
Victor Payno <vpayno@users.noreply.github.com>
2022-03-28 17:35:27 -0700

    algorithims/0007-mini_max_sum: 1st iteration

--- prepare/algorithims/0007-mini_max_sum/src/challenge/main.py
+++ prepare/algorithims/0007-mini_max_sum/src/challenge/main.py
@@ -5,6 +5,7 @@ import os
 import random
 import re
 import sys
+from typing import List
 
 #
 # Complete the 'miniMaxSum' function below.
@@ -12,10 +13,24 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def miniMaxSum(arr):
-    # Write your code here
 
-if __name__ == '__main__':
+def miniMaxSum(arr):
+    arr.sort()
+
+    slice_size = max(len(arr) - 1, 0)
+
+    slice_min: List[int] = arr[0:slice_size]
+    slice_max: List[int] = arr[-slice_size:]
+
+    sum_min: int = sum(slice_min)
+    sum_max: int = sum(slice_max)
+
+    results = (sum_min, sum_max)
+
+    print(" ".join([str(number) for number in results]))
+
+
+if __name__ == "__main__":
 
     arr = list(map(int, input().rstrip().split()))
 
