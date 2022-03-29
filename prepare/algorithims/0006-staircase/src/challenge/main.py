257e183efc37f132bc3e8b0f3f2a110aafaa4347
Victor Payno <vpayno@users.noreply.github.com>
2022-03-28 17:15:38 -0700

    algorithims/0006-staircase: 1st iteration

--- prepare/algorithims/0006-staircase/src/challenge/main.py
+++ prepare/algorithims/0006-staircase/src/challenge/main.py
@@ -6,16 +6,22 @@ import random
 import re
 import sys
 
-#
-# Complete the 'staircase' function below.
-#
-# The function accepts INTEGER n as parameter.
-#
 
 def staircase(n):
-    # Write your code here
+    """
+    Its base and height are both equal to . It is drawn using # symbols and spaces.
+    The last line is not preceded by any spaces.
+    """
 
-if __name__ == '__main__':
+    char = "#"
+
+    for count in range(1, n + 1):
+        step = char * count
+        line = f"{step: >{n}}"
+        print(line)
+
+
+if __name__ == "__main__":
     n = int(input().strip())
 
     staircase(n)
