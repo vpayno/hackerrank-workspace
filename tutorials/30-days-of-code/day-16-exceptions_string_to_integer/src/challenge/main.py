3380ebd270aae3bc2f16c8ad753ecb94d744198b
Victor Payno <vpayno@users.noreply.github.com>
2022-03-30 19:33:14 -0700

    30-days-of-code/day-16-exceptions_string_to_integer: 1st and only iteration

--- tutorials/30-days-of-code/day-16-exceptions_string_to_integer/src/challenge/main.py
+++ tutorials/30-days-of-code/day-16-exceptions_string_to_integer/src/challenge/main.py
@@ -6,7 +6,21 @@ import random
 import re
 import sys
 
+# LOL, they include this line that breaks tests because conditionals aren't allowed.
+# With every passing day, I'm less impressed with HackerRank.
+# i f __name__ == "__main__":
 
+# Not going to bother with pytests for this one.
 
-if __name__ == '__main__':
-    S = input()
+# Also, why do they keep insisting on importing math, os, random, re, and sys into
+# projects?
+
+# Its rediculous that most of their challenges don't promote good coding or
+# doing things things the way they should be for a language.
+
+S = input()
+
+try:
+    print(int(S))
+except ValueError:
+    print("Bad String")
