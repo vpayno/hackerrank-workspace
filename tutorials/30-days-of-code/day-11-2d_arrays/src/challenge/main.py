bc98ede0710d677539c3933c628de4e199329310
Victor Payno <vpayno@users.noreply.github.com>
2022-03-30 21:14:59 -0700

    30-days-of-code/day-11-2d_arrays: 1st iteration

--- tutorials/30-days-of-code/day-11-2d_arrays/src/challenge/main.py
+++ tutorials/30-days-of-code/day-11-2d_arrays/src/challenge/main.py
@@ -1,74 +1,66 @@
 #!/bin/python3
+"""
+Day 11 - 2D Arrays
+"""
 
-import math
-import os
-import random
-import re
-import sys
-from pprint import pprint
-from typing import List
+# from pprint import pprint
+from typing import Dict, List, Set
 
 
-def solve(numbers: List[int]):
+def solve(matrix: List[List[int]]):
+    """
+    Calculate the hourglass sum for every hourglass in matrix,
+    then print the maximum hourglass sum.
+    """
 
-    sums: set = set()
-    total: int = 0
-    rows: List[int] = []
-    row_start: int = 0
-    row_index: int = 0
-    col_start: int = 0
-    col_index: int = 0
-    line_num: int = 0
-    skip_mid_col: bool = False
-    col_shift: int = 0
-    hourglass_index: int = 0
+    hg_sums: Set = set()
+    hg_sum: int = 0
 
-    for col_start in range(0, 4):
-        total = 0
-        skip_mid_col = False
-        print(f"col_start={col_start}")
-        for row_start in [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]:
+    skip_mid_edge_cell: bool = False
+    middle_line: bool = False
 
-            for col_index in range(col_start, col_start + 3):
-                if row_start > 3 or row_index > 4:
-                    break
+    hg_collection: Dict[int, List[List[int]]] = {}
+    hg_coordinates: List[List[int]] = []
 
-                print(f"skip_mid_col={skip_mid_col}")
-                skip_mid_col = not skip_mid_col
+    row_offset: int = 0
+    column_offset: int = 0
+    row: int = 0
+    column: int = 0
 
-                print(f"col_index={col_index}")
+    for row_offset in range(0, 4):
 
-                row_index = row_start + 0
-                print(f"\trow_index={row_index}={row_index - 0} + 0")
-                total += numbers[row_index][col_index]
-                print(f"\ttotal={total}+={numbers[row_index][col_index]}")
+        for column_offset in range(0, 4):
+            hg_sum = 0
+            hg_coordinates = []
 
-                row_index = row_start + 1
-                print(f"\trow_index={row_index}={row_index - 1} + 1")
-                if not skip_mid_col:
-                    total += numbers[row_index][col_index]
-                    print(f"\ttotal={total}+={numbers[row_index][col_index]}")
+            for row in range(row_offset, row_offset + 3):
 
-                row_index = row_start + 2
-                print(f"\trow_index={row_index}={row_index - 2} + 2")
-                total += numbers[row_index][col_index]
-                print(f"\ttotal={total}+={numbers[row_index][col_index]}")
+                if skip_mid_edge_cell and middle_line:
+                    # print(f"        {matrix[row][column]} ", end="")
+                    # print(f"({row},{column_offset + 1}) ", end="")
+                    hg_coordinates.append([row, column_offset + 1])
+                    hg_sum += matrix[row][column_offset + 1]
+                else:
+                    for column in range(column_offset, column_offset + 3):
+                        # print(f"{matrix[row][column]} ", end="")
+                        # print(f"({row},{column}) ", end="")
+                        hg_coordinates.append([row, column])
+                        hg_sum += matrix[row][column]
 
-                print()
+                # print()
+                skip_mid_edge_cell = not skip_mid_edge_cell
+                middle_line = not middle_line
 
-                hourglass_index == 1
+            # print()
+            middle_line = False
+            skip_mid_edge_cell = False
 
-            if hourglass_index > 2:
-                hourglass_index = 0
-                sums.add(total)
-                print(f"Completed first hourglass. sum={total}")
-                total = 0
-            else:
-                print("next column in hourglass...")
+            hg_sums.add(hg_sum)
+            hg_collection.update({hg_sum: hg_coordinates})
 
-        col_start += 1
-
-    pprint(sums)
+    # pprint(hg_sums)
+    # pprint(hg_collection)
+    print(sorted(list(hg_sums))[-1])
 
 
 if __name__ == "__main__":
@@ -78,6 +70,6 @@ if __name__ == "__main__":
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
-    pprint(arr)
+    # pprint(arr)
 
     solve(arr)
