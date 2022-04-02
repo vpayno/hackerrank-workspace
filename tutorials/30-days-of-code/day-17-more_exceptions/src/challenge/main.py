beaece046c921f635dcc3732c6bac8d710908544
Victor Payno <vpayno@users.noreply.github.com>
2022-04-02 11:04:11 -0700

    30-days-of-code/day-17-more_exceptions: 1st iteration

--- tutorials/30-days-of-code/day-17-more_exceptions/src/challenge/main.py
+++ tutorials/30-days-of-code/day-17-more_exceptions/src/challenge/main.py
@@ -1,4 +1,36 @@
-# Write your code here
+#!/usr/bin/env python3
+
+from typing import Dict
+
+
+class CalculatorError(Exception):
+
+    def __init__(self, key: str = "generic") -> None:
+        self.messages: Dict[str, str] = {}
+        self.message: str
+
+        self.messages["generic"] = "generic calculator error"
+        self.messages["power"] = "n and p should be non-negative"
+
+        self.message = self.messages.get(key, "generic calculator error")
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class Calculator:
+
+    @staticmethod
+    def power(number: int, power: int) -> None:
+        if number >= 0 and power >= 0:
+            result: int = 1
+            for _ in range(0, power):
+                result = result * number
+        else:
+            raise CalculatorError("power")
+
+        return result
+
 
 myCalculator = Calculator()
 T = int(input())
