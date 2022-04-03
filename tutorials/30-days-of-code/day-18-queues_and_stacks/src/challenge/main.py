d588c4b326cc61408a11ba71e48beff58270ec32
Victor Payno <vpayno@users.noreply.github.com>
2022-04-03 10:10:09 -0700

    30-days-of-code/day-18-queues_and_stacks: 1st iteration

--- tutorials/30-days-of-code/day-18-queues_and_stacks/src/challenge/main.py
+++ tutorials/30-days-of-code/day-18-queues_and_stacks/src/challenge/main.py
@@ -1,11 +1,53 @@
+#!/usr/bin/env python3
+
 import sys
+from typing import List
 
 
 class Solution:
-    # Write your code here
+
+    def __init__(self) -> None:
+        self.stack: List[str] = []
+        self.queue: List[str] = []
+
+    def pushCharacter(self, char: str) -> None:
+        """
+        Pushes a character onto a stack.
+
+        Read last to first.
+        """
+
+        self.stack.append(char)
+
+    def enqueueCharacter(self, char: str) -> None:
+        """
+        Enqueues a character in the queue.
+
+        Read first to last.
+        """
+
+        self.queue.append(char)
+
+    def popCharacter(self) -> str:
+        """
+        pops and returns the character at the top of the stack.
+
+        Read last to first.
+        """
+
+        return self.stack.pop()
+
+    def dequeueCharacter(self) -> str:
+        """
+        Dequeues and returns the first character in the queue.
+
+        Read first to last.
+        """
+
+        return self.queue.pop(0)
 
 
-    # read the string s
+# read the string s
 s = input()
 # Create the Solution class object
 obj = Solution()
@@ -28,6 +70,6 @@ for i in range(l // 2):
         break
 # finally print whether string s is palindrome or not.
 if isPalindrome:
-    print("The word, "+s+", is a palindrome.")
+    print("The word, " + s + ", is a palindrome.")
 else:
-    print("The word, "+s+", is not a palindrome.")
+    print("The word, " + s + ", is not a palindrome.")
