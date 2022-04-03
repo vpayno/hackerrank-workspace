40698dc3026128930e4353092c6257d307b537b4
Victor Payno <vpayno@users.noreply.github.com>
2022-04-03 15:19:23 -0700

    30-days-of-code/day-20-sorting: 1st iteration

--- tutorials/30-days-of-code/day-20-sorting/src/challenge/main.py
+++ tutorials/30-days-of-code/day-20-sorting/src/challenge/main.py
@@ -1,16 +1,37 @@
-#!/bin/python3
+#!/usr/bin/env python3
+"""
+Day 20 - Bubble Sort
+"""
 
-import math
-import os
-import random
-import re
-import sys
+from typing import List
 
+if __name__ == "__main__":
+    quantity: int = int(input().strip())
+    numbers: List[int] = list(map(int, input().rstrip().split(" ")))
 
+    i: int
+    j: int
 
-if __name__ == '__main__':
-    n = int(input().strip())
+    num_swaps: int = 0
+    first_element: int
+    last_element: int
 
-    a = list(map(int, input().rstrip().split()))
+    for i in range(0, len(numbers)):
 
-    # Write your code here
+        for j in range(0, len(numbers) - i - 1):
+
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+                num_swaps += 1
+
+        if num_swaps == 0:
+            break
+
+    first_element = numbers[0]
+    last_element = numbers[-1]
+
+    print(f"sorted numbers={numbers}")
+
+    print(f"Array is sorted in {num_swaps} swaps.")
+    print(f"First Element: {first_element}")
+    print(f"Last Element: {last_element}")
