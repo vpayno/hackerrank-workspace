fa91b323ccacab5cdaf527b51b75ecdd0ed3b3be
Victor Payno <vpayno@users.noreply.github.com>
2022-04-03 13:34:45 -0700

    30-days-of-code/day-19-interfaces: 1st iteration

--- tutorials/30-days-of-code/day-19-interfaces/src/challenge/main.py
+++ tutorials/30-days-of-code/day-19-interfaces/src/challenge/main.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 
 class AdvancedArithmetic(object):
 
-    def divisorSum(n):
+    def divisorSum(self, n: int) -> int:
         raise NotImplementedError
 
 
 class Calculator(AdvancedArithmetic):
 
-    def divisorSum(self, n):
-        pass
+    def divisorSum(self, n: int) -> int:
+
+        number: int = n
+        multiples: List[int] = []
+
+        for divisor in range(1, number + 1):
+            if number % divisor == 0:
+                multiples.append(divisor)
+
+        return sum(multiples)
 
 
-n = int(input())
-my_calculator = Calculator()
-s = my_calculator.divisorSum(n)
+n: int = int(input())
+my_calculator: Calculator = Calculator()
+s: int = my_calculator.divisorSum(n)
 print("I implemented: " + type(my_calculator).__bases__[0].__name__)
 print(s)
