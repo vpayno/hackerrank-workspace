f7e2fd0ebd71842d6f987970b50f15167d3af825
Victor Payno <vpayno@users.noreply.github.com>
2022-04-05 08:49:20 -0700

    30-days-of-code/day-21-generics: 1st iteration

--- tutorials/30-days-of-code/day-21-generics/src/challenge/main.cpp
+++ tutorials/30-days-of-code/day-21-generics/src/challenge/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ostream>
 
 using namespace std;
 
@@ -10,23 +11,39 @@ using namespace std;
 *    @param A generic vector
 **/
 
-// Write your code here
+template <typename T>
+ostream& operator<< (ostream& out, const vector<T>& v) {
+	for(size_t i = 0; i < v.size(); ++i) {
+		out << v[i] << endl;
+	}
+
+	return out;
+}
+
+template <typename T>
+void printArray(std::vector<T>& v) {
+	cout << v;
+}
 
 int main() {
 	int n;
-	
+
+	// 1st input: number of intergers
 	cin >> n;
 	vector<int> int_vector(n);
 	for (int i = 0; i < n; i++) {
 		int value;
+		// 2nd input(s): n integers, 1 per line
 		cin >> value;
 		int_vector[i] = value;
 	}
-	
+
+	// 3rd input: number of strings
 	cin >> n;
 	vector<string> string_vector(n);
 	for (int i = 0; i < n; i++) {
 		string value;
+		// 4th input(s): n strings, 1 per line
 		cin >> value;
 		string_vector[i] = value;
 	}
