1558f7d901ea9fe9d09de945899066f280ad6ffb
Victor Payno <vpayno@users.noreply.github.com>
2022-04-09 20:42:55 -0700

    30-days-of-code/day-24-more_linked_lists: 1st iteration

--- tutorials/30-days-of-code/day-24-more_linked_lists/src/challenge/main.py
+++ tutorials/30-days-of-code/day-24-more_linked_lists/src/challenge/main.py
@@ -1,34 +1,90 @@
-class Node:
-    def __init__(self,data):
+#!/usr/bin/env python3
+"""
+Day 24: More Linked Lists
+"""
+
+
+class Node:  # pylint: disable=too-few-public-methods
+    """
+    Node class
+    """
+
+    def __init__(self, data):
         self.data = data
-        self.next = None 
-class Solution: 
-    def insert(self,head,data):
-            p = Node(data)           
-            if head==None:
-                head=p
-            elif head.next==None:
-                head.next=p
-            else:
-                start=head
-                while(start.next!=None):
-                    start=start.next
-                start.next=p
-            return head  
-    def display(self,head):
+        self.next = None
+
+
+class Solution:
+    """
+    Challenge solution
+    """
+
+    @staticmethod
+    def insert(head, data):
+        """
+        Inserts a node into a linked list.
+        """
+
+        p = Node(data)
+
+        if head == None:
+            head = p
+        elif head.next == None:
+            head.next = p
+        else:
+            start = head
+            while start.next != None:
+                start = start.next
+            start.next = p
+
+        return head
+
+    @staticmethod
+    def display(head):
+        """
+        Print the linked list.
+        """
+
         current = head
+
         while current:
-            print(current.data,end=' ')
+            print(current.data, end=" ")
             current = current.next
 
-    def removeDuplicates(self,head):
-        #Write your code here
+    @staticmethod
+    def removeDuplicates(head):
+        """
+        Remove duplicates from a node list.
+        """
 
-mylist= Solution()
-T=int(input())
-head=None
+        items = []
+
+        current = head
+        previous = None
+
+        while current:
+            # print(f"examinig {current.data}")
+
+            if current.data in items:
+                # print("removing duplicate")
+                previous.next = current.next
+            else:
+                # print("adding number to items")
+                items.append(current.data)
+                previous = current
+
+            current = current.next
+
+        return head
+
+
+mylist = Solution()
+T = int(input())
+head = None
+
 for i in range(T):
-    data=int(input())
-    head=mylist.insert(head,data)    
-head=mylist.removeDuplicates(head)
-mylist.display(head);
+    data = int(input())
+    head = mylist.insert(head, data)
+
+head = mylist.removeDuplicates(head)
+mylist.display(head)
