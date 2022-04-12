0d1a8f9c9ab17dd0e39e2e62759bfe599911e660
Victor Payno <vpayno@users.noreply.github.com>
2022-04-11 19:53:31 -0700

    30-days-of-code/day-28-regex_patterns_intro_to_databases: 1st iteration

--- tutorials/30-days-of-code/day-28-regex_patterns_intro_to_databases/src/challenge/main.py
+++ tutorials/30-days-of-code/day-28-regex_patterns_intro_to_databases/src/challenge/main.py
@@ -1,15 +1,15 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
-import math
-import os
-import random
 import re
-import sys
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     N = int(input().strip())
+
+    database = {}
+
+    names = []
+
+    reo = re.compile(r"^.*@gmail.com$")
 
     for N_itr in range(N):
         first_multiple_input = input().rstrip().split()
@@ -17,3 +17,14 @@ if __name__ == '__main__':
         firstName = first_multiple_input[0]
 
         emailID = first_multiple_input[1]
+
+        database[emailID] = firstName
+
+    for email, name in database.items():
+        if reo.match(email):
+            names.append(name)
+
+    names.sort()
+
+    for name in names:
+        print(f"{name}")
