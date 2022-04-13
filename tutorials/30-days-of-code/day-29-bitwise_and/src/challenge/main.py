21dcd91fe85884fce0f4a559b24e7416cdc40983
Victor Payno <vpayno@users.noreply.github.com>
2022-04-12 22:07:23 -0700

    30-days-of-code/day-29-bitwise_and: 1st iteration - too slow with test cases 2, 3, 4

--- tutorials/30-days-of-code/day-29-bitwise_and/src/challenge/main.py
+++ tutorials/30-days-of-code/day-29-bitwise_and/src/challenge/main.py
@@ -1,25 +1,33 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
-import math
 import os
-import random
-import re
-import sys
+from typing import Set
 
-#
-# Complete the 'bitwiseAnd' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER N
-#  2. INTEGER K
-#
 
-def bitwiseAnd(N, K):
-    # Write your code here
+def bitwiseAnd(N: int, K: int) -> int:
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    results: Set = set()
+    result: int
+
+    i: int
+    j: int
+
+    for i in range(1, N):
+        for j in range(i + 1, N + 1):
+            # print(f"{i} & {j} = {i & j}")
+            results.add(i & j)
+
+    for i in reversed(list(results)):
+        if i < K:
+            result = i
+            break
+
+    # print(f"result={result}")
+    return result
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     t = int(input().strip())
 
@@ -32,6 +40,6 @@ if __name__ == '__main__':
 
         res = bitwiseAnd(count, lim)
 
-        fptr.write(str(res) + '\n')
+        fptr.write(str(res) + "\n")
 
     fptr.close()
