519d21b93e1ebd7acc3e8e0fe981a1da9d8ceb58
Victor Payno <vpayno@users.noreply.github.com>
2022-04-19 20:15:57 -0700

    30-days-of-code/day-23-bts_level_order_traversal: 1st iteration

--- tutorials/30-days-of-code/day-23-bts_level_order_traversal/src/challenge/main.py
+++ tutorials/30-days-of-code/day-23-bts_level_order_traversal/src/challenge/main.py
@@ -1,29 +1,53 @@
+#!/usr/bin/env python3
+
 import sys
 
+
 class Node:
-    def __init__(self,data):
-        self.right=self.left=None
+
+    def __init__(self, data):
+        self.right = self.left = None
         self.data = data
+
+
 class Solution:
-    def insert(self,root,data):
-        if root==None:
+
+    def insert(self, root, data):
+        if root == None:
             return Node(data)
         else:
-            if data<=root.data:
-                cur=self.insert(root.left,data)
-                root.left=cur
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
             else:
-                cur=self.insert(root.right,data)
-                root.right=cur
+                cur = self.insert(root.right, data)
+                root.right = cur
         return root
 
-    def levelOrder(self,root):
-        #Write your code here
+    def levelOrder(self, root):
+        results = []
+        queue = []
 
-T=int(input())
-myTree=Solution()
-root=None
+        current = root
+
+        while current is not None:
+            results.append(f"{current.data}")
+            if current.left is not None:
+                queue.append(current.left)
+            if current.right is not None:
+                queue.append(current.right)
+            try:
+                current = queue.pop(0)
+            except IndexError:
+                break
+
+        print(" ".join(results))
+
+
+T = int(input())
+myTree = Solution()
+root = None
 for i in range(T):
-    data=int(input())
-    root=myTree.insert(root,data)
+    data = int(input())
+    root = myTree.insert(root, data)
 myTree.levelOrder(root)
