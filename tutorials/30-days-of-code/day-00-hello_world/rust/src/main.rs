adf6c46433ba47af08a2d4d728db8c74585d60e7
Victor Payno <vpayno@users.noreply.github.com>
2022-04-21 20:16:31 -0700

    30-days-of-code/day-00-hello_world/rust: 1st iteration

--- tutorials/30-days-of-code/day-00-hello_world/rust/src/main.rs
+++ tutorials/30-days-of-code/day-00-hello_world/rust/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
+fn get_user_input() -> String {
+    let mut buffer: String = String::new();
+
+    let stdin = io::stdin();
+
+    stdin.read_line(&mut buffer).ok();
+
+    return buffer;
+}
+
+fn print_user_message(message: String) {
+    // Don't print an extra new line since the new line the input has one.
+    print!("{message}", message = message);
+}
+
+fn print_hello_world() {
+    let mut message: String = String::new();
+
+    message.push_str("Hello, World.");
+
+    println!("{greeting}", greeting = message);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let message = get_user_input();
+
+    print_hello_world();
+    print_user_message(message);
 }
